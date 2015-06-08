3ec2a5cccfb03be2a59466201fa5c7fabd5c1f41
Mark McGuire <mmcguire@backstopsolutions.com>
2015-06-08 09:24:17 -0500

    fix spelling of chown

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,7 +10,7 @@ class wildfly::install  {
   }
   ~>
   exec { 'Unpack wildfly':
-    command     => "mkdir -p ${wildfly::dirname} && tar --no-same-owner --no-same-permissions -xzf /usr/src/wildfly-${wildfly::version}.tar.gz -C ${wildfly::dirname} && chwon -R ${wildfly::user}:${wildfly::group} ${wildfly::dirname}",
+    command     => "mkdir -p ${wildfly::dirname} && tar --no-same-owner --no-same-permissions -xzf /usr/src/wildfly-${wildfly::version}.tar.gz -C ${wildfly::dirname} && chown -R ${wildfly::user}:${wildfly::group} ${wildfly::dirname}",
     refreshonly => true
   }
 }
