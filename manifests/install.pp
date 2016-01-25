32ac9cbe453b4d4884489ff3d240b33e1aba0b39
Mark McGuire <mmcguire@backstopsolutions.com>
2016-01-25 11:34:07 -0600

    fix Unpack wildfly command to remove old installations of wildfly

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,7 +10,8 @@ class wildfly::install  {
   }
   ~>
   exec { 'Unpack wildfly':
-    command     => "mkdir -p ${wildfly::dirname} && tar --no-same-owner --no-same-permissions -xzf /usr/src/wildfly-${wildfly::version}.tar.gz -C ${wildfly::dirname} --strip-components=1 && chown -R ${wildfly::user}:${wildfly::group} ${wildfly::dirname}",
-    refreshonly => true
+    command     => "mkdir -p ${wildfly::dirname} && rm -rf ${wildfly::dirname}/* && tar --no-same-owner --no-same-permissions -xzf /usr/src/wildfly-${wildfly::version}.tar.gz -C ${wildfly::dirname} --strip-components=1 && chown -R ${wildfly::user}:${wildfly::group} ${wildfly::dirname}",
+    refreshonly => true,
+    notify => Service[$::wildfly::service_name]
   }
 }
